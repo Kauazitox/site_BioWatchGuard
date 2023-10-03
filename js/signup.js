74a8f6c4d3f4d819a7f9fe471ba92738aa9fd404
Kauã Matheus Callegari Ribeiro <100441524+KauaIFPR@users.noreply.github.com>
2023-10-03 20:23:54 -0300

    Update signup.js

--- js/signup.js
+++ js/signup.js
@@ -98,7 +98,7 @@ function cadastrar(){
     msgError.innerHTML = ''
     
     setTimeout(()=>{
-        window.location.href = 'signin.html'
+        window.location.href = 'html/signin.html'
     }, 3000)
   
     
