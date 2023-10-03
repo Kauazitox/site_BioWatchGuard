21a9d8f039118ae425d98f9c64a640bb22151316
Kauã Matheus Callegari Ribeiro <100441524+KauaIFPR@users.noreply.github.com>
2023-10-03 20:28:08 -0300

    Update signin.js

--- js/signin.js
+++ js/signin.js
@@ -41,7 +41,7 @@ function entrar(){
   })
    
   if(usuario.value == userValid.user && senha.value == userValid.senha){
-    window.location.href = '../Site_DesWEB/index.html'
+    window.location.href = 'Site_DesWEB/index.html'
     
     let mathRandom = Math.random().toString(16).substr(2)
     let token = mathRandom + mathRandom
