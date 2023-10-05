e99289f4790c8f3551809b334f1489c1ac61a9c5
Éryca Luiza Obalski <100441520+erycaluiza@users.noreply.github.com>
2023-10-05 14:41:10 -0300

    Update signup.js

--- js/signup.js
+++ js/signup.js
@@ -104,7 +104,7 @@ function cadastrar(){
     
   } else {
     msgError.setAttribute('style', 'display: block')
-    msgError.innerHTML = '<strong>Preencha todos os campos corretamente antes de cadastrar</strong>'
+    msgError.innerHTML = '<strong>Preencha corretamente os campos antes de cadastrar</strong>'
     msgSuccess.innerHTML = ''
     msgSuccess.setAttribute('style', 'display: none')
   }
